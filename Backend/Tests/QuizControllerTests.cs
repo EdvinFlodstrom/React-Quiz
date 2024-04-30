ab8ced1015d882a8156d43590d63015c9636ef7b
Edvin Flodström <edvin.flodstrom@gmail.com>
2024-04-30 14:00:26 +0200

    test(tests): Add QuizController tests

--- Backend/Tests/QuizControllerTests.cs
+++ Backend/Tests/QuizControllerTests.cs
@@ -1,5 +1,6 @@
 using Backend.Controllers;
 using Backend.Handlers.Questions;
+using Backend.Models.Dtos;
 using Backend.Models.Entities.QuestionTypes;
 using FluentAssertions;
 using MediatR;
@@ -32,9 +33,91 @@ public class QuizControllerTests
     [TestMethod]
     public async Task CreateQuestion_Should_Return_FourOptionQuestionDto()
     {
-        //Arrange
-        string questionType = "Math";
-        MathQuestion question = new()
+        // Arrange
+        string questionType = "Geography";
+        MathQuestion questionRequest = new()
+        {
+            Question = "What is Eyjafjallajökull?",
+            Option1 = "A glacier in Norway",
+            Option2 = "A volcano on Iceland",
+            Option3 = "A crater in China",
+            Option4 = "A city on Greenland",
+            CorrectOptionNumber = 2,
+        };
+
+        CreateQuestionCommandResponse commandResponse = new()
+        {
+            Question = new()
+            {
+                Question = questionRequest.Question,
+                Option1 = questionRequest.Option1,
+                Option2 = questionRequest.Option2,
+                Option3 = questionRequest.Option3,
+                Option4 = questionRequest.Option4,
+            },
+            Success = true,
+            Error = null,
+        };
+
+        string jsonString = JsonSerializer.Serialize(questionRequest, options);
+        JsonDocument jsonDocument = JsonDocument.Parse(jsonString);
+        JsonElement fourOptionQuestionJson = jsonDocument.RootElement;
+
+        _mediatorMock.Setup(m => m.Send(It.IsAny<CreateQuestionCommand>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(commandResponse);
+
+        // Act
+        var response = await _controller.CreateQuestion(questionType, fourOptionQuestionJson);
+        _mediatorMock.Reset();
+
+        // Assert
+        response.Should().NotBeNull();
+        response.Result.Should().NotBeNull();
+        response.Result.Should().BeOfType<OkObjectResult>();
+
+        ObjectResult objectResult = (ObjectResult)response.Result!;
+        objectResult.StatusCode.Should().Be(200);
+
+        FourOptionQuestionDto question = (FourOptionQuestionDto)objectResult.Value!;
+        question.Should().NotBeNull();
+        question.Question.Should().Be("What is Eyjafjallajökull?");
+        question.Option1.Should().Be("A glacier in Norway");
+        question.Option2.Should().Be("A volcano on Iceland");
+        question.Option3.Should().Be("A crater in China");
+        question.Option4.Should().Be("A city on Greenland");
+    }
+
+    [TestMethod]
+    public async Task CreateQuestion_QuestionInvalid_Should_Return_()
+    {
+        // Arrange
+        string questionType = "Geography";
+        MathQuestion questionRequest = new()
+        {
+            Question = "What is Eyjafjallajökull?",
+            Option1 = "A glacier in Norway",
+            Option2 = "A volcano on Iceland",
+            Option3 = "A crater in China",
+            Option4 = "A city on Greenland",
+            CorrectOptionNumber = 1400,
+        };
+
+        string jsonString = JsonSerializer.Serialize(questionRequest, options);
+        JsonDocument jsonDocument = JsonDocument.Parse(jsonString);
+        JsonElement fourOptionQuestionJson = jsonDocument.RootElement;
+
+        // Act
+        var response = await _controller.CreateQuestion(questionType, fourOptionQuestionJson);
+
+        // Assert
+    }
+
+    [TestMethod]
+    public async Task CreateQuestion_QuestionTypeNull_Should_Return_BadRequest()
+    {
+        // Arrange
+        string? questionType = null;
+        MathQuestion questionRequest = new()
         {
             Question = "What is Eyjafjallajökull?",
             Option1 = "A glacier in Norway",
@@ -43,39 +126,81 @@ public class QuizControllerTests
             Option4 = "A city on Greenland",
             CorrectOptionNumber = 2
         };
-
-        CreateQuestionCommandResponse questionResponse = new()
-        {
-            Question = new()
-            {
-                Question = question.Question,
-                Option1 = question.Option1,
-                Option2 = question.Option2,
-                Option3 = question.Option3,
-                Option4 = question.Option4,
-            },
-            Success = true,
-            Error = null,
-        };
-
-        string jsonString = JsonSerializer.Serialize(question);
+        string jsonString = JsonSerializer.Serialize(questionRequest);
         JsonDocument jsonDocument = JsonDocument.Parse(jsonString);
         JsonElement fourOptionQuestionJson = jsonDocument.RootElement;
 
-        CreateQuestionCommand command = new()
-        { FourOptionQuestion = question };
-
-        _mediatorMock.Setup(m => m.Send(It.IsAny<CreateQuestionCommand>(), It.IsAny<CancellationToken>()))
-            .ReturnsAsync(questionResponse);
-
-        //Act
+        // Act
+#pragma warning disable CS8604 // Possible null reference argument.
         var response = await _controller.CreateQuestion(questionType, fourOptionQuestionJson);
+#pragma warning restore CS8604 // Possible null reference argument.
 
-        //Assert
+        // Assert
         response.Should().NotBeNull();
         response.Result.Should().NotBeNull();
-        response.Result.Should().BeOfType<ObjectResult>();
+        response.Result.Should().BeOfType<BadRequestObjectResult>();
 
-        // Add additional assertions, related to response.Result
+        ObjectResult objectResult = (ObjectResult)response.Result!;
+        objectResult.StatusCode.Should().Be(400);
+    }
+
+    [TestMethod]
+    public async Task CreateQuestion_QuestionTypeEmpty_Should_Return_BadRequest()
+    {
+        // Arrange
+        string? questionType = "";
+        MathQuestion questionRequest = new()
+        {
+            Question = "What is Eyjafjallajökull?",
+            Option1 = "A glacier in Norway",
+            Option2 = "A volcano on Iceland",
+            Option3 = "A crater in China",
+            Option4 = "A city on Greenland",
+            CorrectOptionNumber = 2
+        };
+        string jsonString = JsonSerializer.Serialize(questionRequest);
+        JsonDocument jsonDocument = JsonDocument.Parse(jsonString);
+        JsonElement fourOptionQuestionJson = jsonDocument.RootElement;
+
+        // Act
+        var response = await _controller.CreateQuestion(questionType, fourOptionQuestionJson);
+
+        // Assert
+        response.Should().NotBeNull();
+        response.Result.Should().NotBeNull();
+        response.Result.Should().BeOfType<BadRequestObjectResult>();
+
+        ObjectResult objectResult = (ObjectResult)response.Result!;
+        objectResult.StatusCode.Should().Be(400);
+    }
+
+    [TestMethod]
+    public async Task CreateQuestion_QuestionTypeInvalid_Should_Return_BadRequest()
+    {
+        // Arrange
+        string? questionType = "My special question type";
+        MathQuestion questionRequest = new()
+        {
+            Question = "What is Eyjafjallajökull?",
+            Option1 = "A glacier in Norway",
+            Option2 = "A volcano on Iceland",
+            Option3 = "A crater in China",
+            Option4 = "A city on Greenland",
+            CorrectOptionNumber = 2
+        };
+        string jsonString = JsonSerializer.Serialize(questionRequest);
+        JsonDocument jsonDocument = JsonDocument.Parse(jsonString);
+        JsonElement fourOptionQuestionJson = jsonDocument.RootElement;
+
+        // Act
+        var response = await _controller.CreateQuestion(questionType, fourOptionQuestionJson);
+
+        // Assert
+        response.Should().NotBeNull();
+        response.Result.Should().NotBeNull();
+        response.Result.Should().BeOfType<BadRequestObjectResult>();
+
+        ObjectResult objectResult = (ObjectResult)response.Result!;
+        objectResult.StatusCode.Should().Be(400);
     }
 }
