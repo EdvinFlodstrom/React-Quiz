617c7af747e56be08170d80a5a558c179dd80d31
Edvin Flodström <edvin.flodstrom@gmail.com>
2024-05-09 13:15:22 +0200

    feat(frontend): Add background flash animation

--- frontend/src/components/TakeQuiz.jsx
+++ frontend/src/components/TakeQuiz.jsx
@@ -1,11 +1,14 @@
 import React, { useRef, useState } from 'react';
 import '../styles/timer.css';
+import '../styles/takeQuiz.css';
 
 const TakeQuiz = ({ playerName }) => {
     const [timerExpired, setTimerExpired] = useState(false);
     const [timerStarted, setTimerStarted] = useState(false);
-    const [canGetQuestion, setCanGetQuestion] = useState(true);
-    const [canSubmitAnswer, setCanSubmitAnswer] = useState(false);
+    const [getQuestionButtonDisabled, setQuestionButtonDisabled] =
+        useState(false);
+    const [submitAnswerButtonDisabled, setsubmitAnswerButtonDisabled] =
+        useState(true);
     const [question, setQuestion] = useState('');
     const [questionOptions, setQuestionOptions] = useState({
         option1: '',
@@ -13,6 +16,7 @@ const TakeQuiz = ({ playerName }) => {
         option3: '',
         option4: '',
     });
+    const [backgroundFlash, setBackGroundFlash] = useState('');
     let timerRef = useRef(null);
 
     const startTimer = () => {
@@ -25,6 +29,7 @@ const TakeQuiz = ({ playerName }) => {
     };
 
     const handleGetQuestion = async () => {
+        setQuestionButtonDisabled(true);
         try {
             const response = await fetch(
                 'https://localhost:7030/api/quiz/get',
@@ -44,7 +49,6 @@ const TakeQuiz = ({ playerName }) => {
             }
 
             const responseData = await response.json();
-            console.log('response data:', responseData);
 
             if (responseData.fourOptionQuestion) {
                 // Question data is received
@@ -58,7 +62,7 @@ const TakeQuiz = ({ playerName }) => {
                     option3: responseData.fourOptionQuestion.option3,
                     option4: responseData.fourOptionQuestion.option4,
                 });
-                setCanSubmitAnswer(true);
+                setsubmitAnswerButtonDisabled(false);
                 startTimer(); // Start the timer after receiving the question
             } else {
                 // Question data is not received
@@ -73,15 +77,48 @@ const TakeQuiz = ({ playerName }) => {
         return new Promise((resolve) => setTimeout(resolve, ms));
     };
 
-    const handleAnswer = (e) => {
+    const handleAnswer = async (answer) => {
+        setsubmitAnswerButtonDisabled(true);
         console.log('Stop timer.');
         setTimerExpired(true);
         setTimerStarted(false);
         clearTimeout(timerRef.current);
+
+        try {
+            const response = await fetch(
+                'https://localhost:7030/api/quiz/answer',
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        playerName: playerName,
+                        questionAnswer: answer,
+                    }),
+                }
+            );
+
+            if (!response.ok) {
+                throw new Error('Failed to check answer');
+            }
+
+            const responseData = await response.json();
+
+            if (responseData.correct) {
+                setBackGroundFlash('flashGreen');
+            } else {
+                setBackGroundFlash('flashRed');
+            }
+            await delay(1000);
+            setBackGroundFlash('');
+        } catch (error) {
+            console.error('Error while checking answer:', error);
+        }
     };
 
     return (
-        <>
+        <div id='flash' className={backgroundFlash}>
             <h1>
                 <span className='slide-in'>Take Quiz</span>
             </h1>
@@ -96,27 +133,42 @@ const TakeQuiz = ({ playerName }) => {
             </div>
 
             <div className='centered-buttons-container'>
-                <button className='button' onClick={handleGetQuestion}>
+                <button
+                    className='button'
+                    onClick={handleGetQuestion}
+                    disabled={getQuestionButtonDisabled}>
                     Get Question
                 </button>
                 <h2>Q: {question}</h2>
 
                 <div className='take-quiz-options-buttons-container'>
-                    <button className='button take-quiz-options-button'>
+                    <button
+                        className='button take-quiz-options-button'
+                        onClick={() => handleAnswer(1)}
+                        disabled={submitAnswerButtonDisabled}>
                         1: {questionOptions.option1}
                     </button>
-                    <button className='button take-quiz-options-button'>
+                    <button
+                        className='button take-quiz-options-button'
+                        onClick={() => handleAnswer(2)}
+                        disabled={submitAnswerButtonDisabled}>
                         2: {questionOptions.option2}
                     </button>
-                    <button className='button take-quiz-options-button'>
+                    <button
+                        className='button take-quiz-options-button'
+                        onClick={() => handleAnswer(3)}
+                        disabled={submitAnswerButtonDisabled}>
                         3: {questionOptions.option3}
                     </button>
-                    <button className='button take-quiz-options-button'>
+                    <button
+                        className='button take-quiz-options-button'
+                        onClick={() => handleAnswer(4)}
+                        disabled={submitAnswerButtonDisabled}>
                         4: {questionOptions.option4}
                     </button>
                 </div>
             </div>
-        </>
+        </div>
     );
 };
 
