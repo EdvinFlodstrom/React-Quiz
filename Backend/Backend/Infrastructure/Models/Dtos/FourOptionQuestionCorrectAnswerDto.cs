4012a2deb53a9da3ff4b6574283847e0d22974a3
Edvin Flodström <edvin.flodstrom@gmail.com>
2024-05-11 12:36:11 +0200

    chore(backend): Remove unused class

--- Backend/Backend/Infrastructure/Models/Dtos/FourOptionQuestionCorrectAnswerDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Backend.Infrastructure.Models.Dtos;
-
-public class FourOptionQuestionCorrectAnswerDto
-{
-    public required string Id { get; set; }
-
-    public required string CorrectOptionNumber { get; set; }
-}
