8052c5c8d238fd7a37c02f308f762ad2f9443f8e
Edvin Flodström <edvin.flodstrom@gmail.com>
2024-05-12 09:19:32 +0200

    chore(frontend): Add ModifyQuestion.jsx logic

--- frontend/src/components/ModifyQuestion.jsx
+++ frontend/src/components/ModifyQuestion.jsx
@@ -1,10 +1,28 @@
 import { useState } from 'react';
 import GetQuestionByIdForm from './GetQuestionByIdForm';
+import QuestionForm from './QuestionForm';
 
 const ModifyQuestion = ({ adjustGradient }) => {
     const [getQuestionByIdButtonDisabled, setGetQuestionByIdButtonDisabled] =
         useState(true);
     const [questionId, setQuestionId] = useState(0);
+    const [getQuestionMessageAndState, setGetQuestionMessageAndState] =
+        useState({
+            success: true,
+            message: '',
+        });
+    const [formData, setFormData] = useState({
+        questionType: '',
+        question: '',
+        option1: '',
+        option2: '',
+        option3: '',
+        option4: '',
+        correctOptionNumber: 1,
+    });
+    const [modifyQuestionButtonDisabled, setModifyQuestionButtonDisabled] =
+        useState(true);
+    const [correctOptionNumber, setCorrectOptionNumber] = useState(1);
 
     const handleGetQuestionFormChange = (e) => {
         const { value } = e.target;
@@ -15,7 +33,53 @@ const ModifyQuestion = ({ adjustGradient }) => {
         setGetQuestionByIdButtonDisabled(intValue <= 0);
     };
 
-    const handleGetQuestion = (e) => {
+    const handleGetQuestion = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch(
+                `https://localhost:7030/api/quiz/get/${questionId}`,
+                {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                }
+            );
+
+            if (response.ok) {
+                adjustGradient();
+                setGetQuestionMessageAndState({
+                    success: true,
+                    message: 'Sucesss!',
+                });
+                const deserializedResponse = await response.json();
+                console.log(deserializedResponse.question);
+                console.log(deserializedResponse.option1);
+                console.log(deserializedResponse.option2);
+                console.log(deserializedResponse.option3);
+                console.log(deserializedResponse.option4);
+                console.log(deserializedResponse.correctOptionNumber);
+            } else {
+                const error = await response.text();
+                setGetQuestionMessageAndState({
+                    success: false,
+                    message: error,
+                });
+                console.error('Failed to create question:', error);
+            }
+        } catch (exception) {
+            setGetQuestionMessageAndState({
+                success: false,
+                message: exception.message,
+            });
+            console.error('An error occured:', exception.message);
+        }
+    };
+
+    const handleFormChange = () => {};
+
+    const handleFormSubmit = async (e) => {
         e.preventDefault();
     };
 
@@ -31,6 +95,26 @@ const ModifyQuestion = ({ adjustGradient }) => {
                 questionId={questionId}
                 handleGetQuestionFormChange={handleGetQuestionFormChange}
                 getQuestionByIdButtonDisabled={getQuestionByIdButtonDisabled}
+            />
+
+            <p
+                className={
+                    getQuestionMessageAndState.success
+                        ? 'success-message'
+                        : 'error-message'
+                }>
+                {getQuestionMessageAndState.message}
+            </p>
+
+            <QuestionForm
+                formData={formData}
+                handleFormChange={handleFormChange}
+                createOrModifyQuestionButtonDisabled={
+                    modifyQuestionButtonDisabled
+                }
+                handleFormSubmit={handleFormSubmit}
+                correctOptionNumber={correctOptionNumber}
+                isModifyingQuestion={true}
             />
         </>
     );
